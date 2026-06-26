aaf1fd8cf7c18c68dc5b65633554ea731fca3d6c
Boris Alexeev <boris.alexeev@gmail.com>
2026-06-26 08:27:08 -0400

    rm Towers.lean

--- src/latest_Submission/Towers.lean
+++
@@ -1,8 +0,0 @@
-import Submission.Algebra.All
-import Submission.CantEick.All
-import Submission.ClassField.All
-import Submission.FieldTheory.All
-import Submission.Geometry.All
-import Submission.Group.All
-import Submission.NumberTheory
-import Submission.Topology.All
